0972b18fbbeceaec78b1d0ff73f696e063815a3a
Uzee11 <uzeeokoye@yahoo.com>
2023-09-21 03:23:48 -0700

    A dream doesn't become reality through magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -13,6 +13,9 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
+
+
+
 	/*
 	 * write your line of code here...
 	 * Remember:
@@ -23,7 +26,7 @@ int main(void)
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
+	printf("a[2] = %d\n" a[2]);
 	return (0);
 }
 

737a42cba8edc1b52b4b412d7952b7ebb553ca2b
Uzee11 <uzeeokoye@.com>
2023-10-23 02:32:45 -0700

    List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * listint_len - Returns number of elements in a linked list
+ * @h: Pointer to a list
+ *
+ * Return: The number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		++i;
+	}
+	return (i);
+}

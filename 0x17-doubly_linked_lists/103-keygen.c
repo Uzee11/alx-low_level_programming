4fbbab3494ed6cba1ecc8437d82e495b3dc4d5ce
Uzee11 <uzeeokoye@yahoo.com>
2023-12-08 00:05:58 -0800

    keygen

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,9 @@
+# crackme5
+
+Create a keygen.
+
+- Usage of the crackme: `./crackme5 username key`
+- The crackme will segfault if you do not enter the correct key for the user
+
+- Usage for your keygen: `./keygen5 username`
+- Your keygen should print a valid key for the `username`

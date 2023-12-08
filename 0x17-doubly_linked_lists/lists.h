95122b9921f9bd3d45a4d6c7b18f64ed240b0d8e
Uzee11 <uzeeokoye@yahoo.com>
2023-12-07 23:29:02 -0800

    102result

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -20,4 +20,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+int is_palindrome(int number);
+int find_largest_palindrome(void);
 #endif /* LISTS_H */
